666f88da5ac4a790dcb370ec4e3ef4c5353eb9c0
KumaresanVaf <KumaresanVaf@users.noreply.github.com>
2023-06-14 21:45:08 +0530

    Add project files.

---
+++ DCL_TCL/DCL_TCL/Employee.sql
@@ -0,0 +1,38 @@
+﻿create table Employee (EmployeeId int primary key, 
+						FirestName varchar(50), 
+						lastName varchar(50),
+						Age int, 
+						Department varchar(50));
+
+
+ insert into Employee values  (1,'Ram', 'kumar', 22, 'sales'),
+							 (2,'Arun','kumar',23,'Marketing'),
+							 (3,'Ashok', 'kumar', 21, 'sales'),
+							 (4,'Radha', 'Ravi', 24, 'sales'),
+							 (5,'Ram','Prasanth',21,'sales');
+	
+
+Begin TRANSACTION
+insert into Employee values (6,'Ganesh', 'Venkat', 24, 'sales'),
+							 (7,'Krithic','Vasan',21,'sales');
+SAVE TRANSACTION FirstInsert;
+
+RollBack;
+
+commit;
+
+ROLLBACK TRANSACTION FirstInsert;
+
+Begin TRANSACTION
+UPDATE Employee
+SET Department = 'Project Maneger'
+WHERE EmployeeID = 1;
+SAVE TRANSACTION SecondInsert;
+
+select * from Employee
+
+Begin TRANSACTION theridInsert
+truncate table employee;
+
+ROLLBACK TRANSACTION theridInsert;
+
